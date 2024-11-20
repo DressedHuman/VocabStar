8b0983a7c2b28941007d0b3c877758b49585743c
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-11-20 14:35:47 +0600

    word and meaning pronunciation functionalized

--- src/components/MyVocabs/ShowWord.tsx
+++ src/components/MyVocabs/ShowWord.tsx
@@ -1,25 +1,50 @@
 import { UserVocabType } from "./MyVocabsHome";
 import DeleteIcon from './icons/delete.svg';
 
+declare global {
+    interface Window {
+        responsiveVoice: {
+            speak: (text: string, voice: string, options?: unknown) => void;
+        };
+    }
+};
+
 interface Props {
     word: UserVocabType;
     deleteVocab: () => void;
 };
 
 const ShowWord = ({ word, deleteVocab }: Props) => {
+    const handleWordClick = () => {
+        const speech = new SpeechSynthesisUtterance(word.word);
+        speech.lang = "en-US";
+        speech.rate = 1;
+        speech.pitch = 1;
+        speech.voice = window.speechSynthesis.getVoices().find(voice => voice.voiceURI.includes("Microsoft Susan"))!;
+        window.speechSynthesis.speak(speech);
+    }
+
+    const handleMeaningClick = (meaning: string) => {
+        window.responsiveVoice.speak(meaning,"Bangla Bangladesh Male", {rate: 1.0, pitch: 1.12});
+    }
+
     return (
         <div
             className="w-full relative"
         >
             <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-3 items-center text-lg lg:text-xl border-2 border-border_color rounded-md px-4 py-2 divide-y-[1px] md:divide-y-0 md:divide-x-[1px] divide-border_color capitalize">
-                <h2 className="text-center">{word.word}</h2>
+                <h2
+                    className="text-center cursor-pointer"
+                    onClick={handleWordClick}
+                >{word.word}</h2>
 
                 {/* Meanings */}
                 <div className="col-span-2 text-center divide-y-0 md:divide-y-[1px] divide-border_color pt-2 md:p-0">
                     {
                         word.meanings.map(meaning => <p
                             key={meaning.id}
-                            className="font-hind_siliguri p-1"
+                            className="font-hind_siliguri p-1 cursor-pointer"
+                            onClick={() => handleMeaningClick(meaning.meaning)}
                         >
                             {meaning.meaning}
                         </p>)
